81c1bf1998d7e2a363ec5010355e4247d277ebe2
saniyafarzindeveloper <developerfarzin@gmail.com>
2025-06-19 20:59:05 +0530

    completed nav bar & popluar companion section

--- app/page.tsx
+++ app/page.tsx
@@ -1,9 +1,45 @@
-import React from 'react'
+import CompanionCard from "@/components/CompanionCard";
+import CompanionList from "@/components/CompanionList";
+import CTA from "@/components/CTA";
+import React from "react";
 
 const Page = () => {
   return (
-    <div className='text-2xl underline'>Page</div>
-  )
-}
+    <main>
+      <h1>Popular Companions</h1>
+      <section className="home-section">
+        <CompanionCard 
+        id="111"
+        name= "Basic Math class 9th"
+        topic="Geometry Chapter 01"
+        subject= "Science"
+        duration ={50}
+        color="#825acc"
+        />
+        <CompanionCard 
+        id="890"
+        name= "Basic Math class 9th"
+        topic="Geometry Chapter 01"
+        subject= "Math"
+        duration ={50}
+        color="#fe9032"
+        />
+        <CompanionCard 
+        id="222"
+        name= "Basic Math class 9th"
+        topic="Geometry Chapter 01"
+        subject= "Physics"
+        duration ={50}
+        color="#faaae5"
+        />
+      </section>
 
-export default Page
+      <section className="home-section">
+        <CompanionList />
+        <CTA />
+      </section>
+    </main>
+  );
+};
+
+export default Page;
---
+++ components/CompanionList.tsx
@@ -0,0 +1,3 @@
+export default function CompanionList() {
+  return <div>CompanionList</div>;
+}
