dbd1947fe2b835ea9f5096b3ff41a8f76fc92771
saniyafarzindeveloper <developerfarzin@gmail.com>
2025-07-14 13:30:16 +0530

    clearing up the folder structure

--- components/CompanionCard.tsx
+++ components/CompanionCard.tsx
@@ -22,14 +22,6 @@ export default function CompanionCard({
     <article className="companion-card" style={{ backgroundColor: color }}>
       <div className="flex justify-between items-center">
         <div className="subject-badge">{subject}</div>
-        <button className="companion-bookmark">
-          <Image
-            src="/icons/bookmark.svg"
-            alt="bookmark"
-            width={12.5}
-            height={15}
-          />
-        </button>
       </div>
 
       <h2 className="text-2xl font-bold">
